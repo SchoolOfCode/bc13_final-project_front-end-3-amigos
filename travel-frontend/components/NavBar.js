b92ac4acc4fcd010a2fd7e6c20660698a4f9f3a8
BushraFatimaBF <vglampard@protonmail.com>
2023-01-10 16:04:10 +0000

    added components in normal js

---
+++ travel-frontend/components/NavBar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from "next/Link";
+
+export default function NavBar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            {" "}
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            {" "}
+            <Link href="/favourites">Favourites</Link>
+          </li>
+          <li>
+            {" "}
+            <Link href="/journal">Journal</Link>
+          </li>
+          <li>
+            {" "}
+            <Link href="/about">About Us</Link>
+          </li>
+          <li>
+            {" "}
+            <Link href="/login">Login</Link>
+          </li>
+          <li>
+            {" "}
+            <Link href="/logout">Logout</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
---
+++ travel-frontend/components/ResultsDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ResultCard from './ResultCard'
+
+export default function ResultsDisplay() {
+  return (
+    <div>
+<ResultCard/>
+    </div>
+  )
+}
