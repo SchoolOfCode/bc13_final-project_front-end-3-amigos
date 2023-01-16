4d0d5b115d2782c485293a810909e48423183640
BushraFatimaBF <vglampard@protonmail.com>
2023-01-16 14:31:06 +0000

    handleclick funciton for favourites button completed nearly - just need to get key into the object passed through to the database

--- travel-frontend/components/ApiResultCard.js
+++ travel-frontend/components/ApiResultCard.js
@@ -1,21 +1,49 @@
+import axios from "axios";
 import React from "react";
 
-function ApiResultCard({ title, city, country, suburb, text, image }) {
+// this button will take the data from the current card and send it off to the POST endpoint
+// how do we grab all the card data?
+//
+
+const URL = process.env.NEXT_PUBLIC_POSTGRES_URL
+
+function handleClick({ key, title, city, country, suburb, text, image }) {
+  const newFavourite = {
+    key,
+    title,
+    city,
+    country,
+    suburb,
+    text,
+    image,
+  }
+  console.log("new Favourite:", newFavourite)
+  axios.post(`${URL}/`, newFavourite);
+}
+
+function ApiResultCard({ key, title, city, country, suburb, text, image }) {
   return (
     <div className="card-container group">
-		<div className="single-card">
-      <img src={image}  className="card-image"/>
+      <div className="single-card">
+        <img src={image} className="card-image" />
       </div>
       <div className=" card-gradient-bg">
-			<div className=" card-info-transition">
-				<h3 className="card-title">{title}</h3>
-				<p className="card-city">{city}</p>
-				<p className="card-location">{country}</p>
-				<p className="card-location">{suburb}</p>
-				<p className="card-desc">{text}</p>
-				<button className="card-btn">See More</button>
-    </div>
-    </div>
+        <div className=" card-info-transition">
+          <h3 className="card-title">{title}</h3>
+          <p className="card-city">{city}</p>
+          <p className="card-location">{country}</p>
+          <p className="card-location">{suburb}</p>
+          <p className="card-desc">{text}</p>
+          <button className="card-btn">See More</button>
+          <button
+            className="card-btn"
+            onClick={()=>handleClick({key, title, city, country, suburb, text, image
+            })}
+          >
+            FAVOURITE!
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
