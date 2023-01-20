8c3d47c064133db6fb64236ce3540c7b49cdb6d7
jo <jyotshnacs@gmail.com>
2023-01-20 10:23:43 +0000

    post request

--- travel-frontend/components/FavouritesButton.js
+++ travel-frontend/components/FavouritesButton.js
@@ -5,64 +5,109 @@ render empty heart
 when clicked in render full heart
 level 2: user is signed only fire post route then 
 */
-import React, {useState} from 'react'
-import Image from 'next/image'
-import emptyHeart from '../public/empty-heart.png'
-import fullHeart from '../public/love.png'
-import {useAuthState} from 'react-firebase-hooks/auth'
-import {getAuth} from 'firebase/auth'
-import {app} from '../firebase/firebase'
+import React, { useState } from "react";
+import Image from "next/image";
+import emptyHeart from "../public/empty-heart.png";
+import fullHeart from "../public/love.png";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getAuth } from "firebase/auth";
+import { app } from "../firebase/firebase";
+import axios from "axios";
 
+function FavouritesButton({
+  id,
+  title,
+  city,
+  country,
+  suburb,
+  description,
+  image,
+}) {
+  const auth = getAuth(app);
+  const [user] = useAuthState(auth);
 
-function FavouritesButton() {
-  const auth = getAuth(app)
-  const [user] = useAuthState(auth)
+  const URL = process.env.NEXT_PUBLIC_POSTGRES_URL;
+  const uid = user.uid;
 
-
-const currentlyAFavourite = <Image src={fullHeart} className="p-1 rounded-full bg-opacity-70 bg-off-white" width={40} alt="a clicked favourite button" />
-const notAFavourite = <Image src={emptyHeart} className="p-1 rounded-full bg-opacity-70 bg-off-white" width={40} alt="an unclicked favourite button" />
-
-const [favourite, setFavourite] = useState(false)
-
-// probably need to pass in XID for this 
-const toggleFavourite = () => {
-
-  // Set state for selected favourite
-
-  if(user){
-    setFavourite((favourite) => {
-      if (favourite === true) {
-        console.log("unfavourited clicked!")
-  
-      } 
-      if (favourite === false) {
-        console.log("Favourited!")
-      }
-  
-      // switched favourite state boolean
-      return !favourite;
-    })
-  } if(!user){
-    alert('please log in')
+  async function postRequest(
+    uid,
+    id,
+    title,
+    city,
+    country,
+    suburb,
+    description,
+    image
+  ) {
+    const res = await axios.post(`${URL}`, {
+      uid,
+      id,
+      title,
+      city,
+      country,
+      suburb,
+      description,
+      image,
+    });
+    console.log(res);
   }
-  console.log(user)
+
+  /**  Make an axios delete request with UID, Xid attached to the body &
+               send to home endpoint*/
+  async function deleteRequest(id, uid) {
+    const res = await axios.delete(`${URL}`, { data: { id, uid } });
+    console.log(res);
   }
- 
 
+  const currentlyAFavourite = (
+    <Image
+      src={fullHeart}
+      className="p-1 rounded-full bg-opacity-70 bg-off-white"
+      width={40}
+      alt="a clicked favourite button"
+    />
+  );
+  const notAFavourite = (
+    <Image
+      src={emptyHeart}
+      className="p-1 rounded-full bg-opacity-70 bg-off-white"
+      width={40}
+      alt="an unclicked favourite button"
+    />
+  );
 
+  const [favourite, setFavourite] = useState(false);
 
+  // probably need to pass in XID for this
+  const toggleFavourite = () => {
+    // Set state for selected favourite
+
+    if (user) {
+      setFavourite((favourite) => {
+        if (favourite === true) {
+          console.log("unfavourited clicked!");
+        }
+        if (favourite === false) {
+          console.log("Favourited!");
+        }
+
+        // switched favourite state boolean
+        return !favourite;
+      });
+    }
+    if (!user) {
+      alert("please log in");
+    }
+    console.log(user);
+  };
 
   return (
     <>
-    <button
-    onClick={() => toggleFavourite()}
-    >
-
-    {favourite === true ? currentlyAFavourite : notAFavourite}
-
-    </button>
+      <button onClick={() => toggleFavourite()}>
+        {favourite === true ? currentlyAFavourite : notAFavourite}
+      </button>
     </>
-  )
+  );
 }
 
-export default FavouritesButton
+export default FavouritesButton;
