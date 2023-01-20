b95005bb899a08f7fc4c664d242cdd29772f9455
jo <jyotshnacs@gmail.com>
2023-01-20 10:26:57 +0000

    delete request

--- travel-frontend/components/ApiResultCard.js
+++ travel-frontend/components/ApiResultCard.js
@@ -3,17 +3,16 @@ import React from "react";
 import FavouritesButton from "./FavouritesButton";
 
 // Pull down URL to use inside of handleclick
-const URL = 'https://final-project-backend-d6jk.onrender.com/userfavourites/'
+const URL = "https://final-project-backend-d6jk.onrender.com/userfavourites/";
 
 // Function that pulls data from the card and sends a post request to put it in the database.
-// PROBLEMS: 
-  // the key is currently undefined (but rest of the data coming through OK)
-  // SO: a) is it going to the database? and 
-  // b) if so, which values are missing/what is it being stored as? e.g., what ID is in there? 
+// PROBLEMS:
+// the key is currently undefined (but rest of the data coming through OK)
+// SO: a) is it going to the database? and
+// b) if so, which values are missing/what is it being stored as? e.g., what ID is in there?
 
-  
-  function handleClick({ title, city, country, suburb, description, image }) {
-  const user_id = 4
+function handleClick({ title, city, country, suburb, description, image }) {
+  const user_id = 4;
   const newFavourite = {
     user_id,
     title,
@@ -22,20 +21,28 @@ const URL = 'https://final-project-backend-d6jk.onrender.com/userfavourites/'
     suburb,
     description,
     image,
-
-  }
-  console.log("new Favourite:", newFavourite)
- axios.post(`${URL}`, newFavourite).then(function (response) {
-  console.log(response);
-})
-.catch(function (error) {
-  console.log(error);
-});
-
+  };
+  console.log("new Favourite:", newFavourite);
+  axios
+    .post(`${URL}`, newFavourite)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
-function ApiResultCard({ title, city, country, suburb, description, image, googleLogin }) {
-
+function ApiResultCard({
+  id,
+  title,
+  city,
+  country,
+  suburb,
+  description,
+  image,
+  googleLogin,
+}) {
   return (
     <div className="card-container group">
       <div className="single-card">
@@ -49,7 +56,17 @@ function ApiResultCard({ title, city, country, suburb, description, image, googl
           <p className="card-location">{suburb}</p>
           <p className="card-desc">{description}</p>
           <button className="card-btn">See More</button>
-          <FavouritesButton />
+          <FavouritesButton
+            key={item.xid}
+            id={item.xid}
+            title={item.name}
+            city={item.address.city}
+            country={item.address.country}
+            suburb={item.address.suburb}
+            description={item.wikipedia_extracts.text}
+            image={item.preview.source}
+            postData={postData}
+          />
 
           {/* <Image id={key} src={heart} alt="untoggled favourite button" width={40} height={40}
           onClick={(id)=>{googleLogin(id)}} 
