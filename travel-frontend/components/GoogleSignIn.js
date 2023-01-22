491c2100a43304cc895fd092f5db4cc95c67a99f
jo <jyotshnacs@gmail.com>
2023-01-22 15:08:55 +0000

    changed the URL

--- travel-frontend/components/GoogleSignIn.js
+++ travel-frontend/components/GoogleSignIn.js
@@ -12,27 +12,28 @@ function GoogleSignIn() {
   const router = useRouter();
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
   console.log(user);
-  // console.log(user.user.displayName);
+  console.log(user.user.displayName);
 
-  async function postUserDetails(userDetails) {
-    console.log("postUserDetails(userDetails) is fired");
-    const URL = process.env.NEXT_PUBLIC_POSTGRES_URL;
-    const res = await axios.post(`${URL}`, {
-      username: user.user.displayName,
-      email: user.user.email,
-      uid: user.user.uid,
-      password: "null",
-    });
-    console.log("postUserDetails:-", URL, userDetails);
-    console.log("user details post response", res);
-  }
+  // async function postUserDetails(userDetails) {
+  //   console.log("postUserDetails(userDetails) is fired");
+  //   const URL = process.env.NEXT_PUBLIC_POSTGRES_URL;
+  //   const res = await axios.post(`${URL}`, {
+  //     username: user.user.displayName,
+  //     email: user.user.email,
+  //     uid: user.user.uid,
+  //     password: "null",
+  //   });
+  //   console.log("postUserDetails:-", URL, userDetails);
+  //   console.log("user details post response", res);
+  // }
   // adding useEffect
   useEffect(() => {
     async function User() {
       if (user) {
-        console.log("user @ UseEffect:", user.user.email);
-        console.log("user @ UseEffect:", user.user.displayName);
-        console.log("user @ UseEffect:", user.user.uid);
+        console.log("user @ UseEffect email:", user.user.email);
+        console.log("user @ UseEffect username:", user.user.displayName);
+        console.log("user @ UseEffect uid:", user.user.uid);
+
         const userDetails = {
           username: user.user.displayName,
           email: user.user.email,
@@ -40,14 +41,30 @@ function GoogleSignIn() {
           password: "null",
         };
         console.log(userDetails);
-        await postUserDetails(userDetails);
+
+        async function postUserDetails(userDetails) {
+          console.log("postUserDetails(userDetails) is fired");
+          // const URL = process.env.NEXT_PUBLIC_POSTGRES_URL;
+          const URL = `//xwrxqxhr:wCU0PAsKZeTaKCaGCueQHoyKuJHh0wNn@mel.db.elephantsql.com/xwrxqxhr`;
+          const res = await axios.post(`${URL}`, {
+            username: user.user.displayName,
+            email: user.user.email,
+            uid: user.user.uid,
+            password: "null",
+          });
+          // console.log("postUserDetails:-", URL, userDetails);
+          console.log("user details post response", res);
+        }
+        postUserDetails(userDetails);
         console.log("registration complete");
+
         router.push("/");
       }
     }
     User();
   }, [user]);
   // console.log(user.user.email);
+  console.log(user);
 
   return (
     <div>
