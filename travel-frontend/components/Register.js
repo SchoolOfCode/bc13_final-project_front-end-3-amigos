3ba04f5ae51f6e28e5974c23dbd1ffb0ff3b9162
btxoana <beatrice.stanila@gmail.com>
2023-01-25 16:08:10 +0000

    auth page done

--- travel-frontend/components/Register.js
+++ travel-frontend/components/Register.js
@@ -1,8 +1,8 @@
-import React, {useState} from 'react'
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
-import { getAuth } from '@firebase/auth'
-import { app } from '../firebase/firebase';
-import axios from 'axios';
+import React, { useState } from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { getAuth } from "@firebase/auth";
+import { app } from "../firebase/firebase";
+import axios from "axios";
 /* 
 PLAN:
 have a form for register
@@ -17,84 +17,78 @@ use axios to create a post request to our db with registered user info
 
 */
 
-
-
 function Register() {
-    const auth = getAuth(app);
+  const auth = getAuth(app);
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [username, setUsername] = useState('');
-    const [
-      createUserWithEmailAndPassword,
-      user,
-      loading,
-      error,
-    ] = useCreateUserWithEmailAndPassword(auth);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
 
-    if (error) {
-        return (
-          <div>
-            <p>Error: {error.message}</p>
-          </div>
-        );
-      }
-      if (loading) {
-        return <p>Loading...</p>;
-      }
-      if (user) {
-        return (
-          <div>
-            <p>Registered User: {user.user.email}</p>
-          </div>
-        );
-      }
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (user) {
+    return (
+      <div>
+        <p>Registered User: {user.user.email}</p>
+      </div>
+    );
+  }
 
-      console.log(email, password, username)
-      
+  console.log(email, password, username);
 
-    async function sendRegistrationDetails(data){
-        const postURL = process.env.NEXT_PUBLIC_POST_URL
-        return await axios.post(postURL, data )
-    }
-    async function postData(){
-        let userObj = {email, username}
-        await sendRegistrationDetails(userObj)
-    }
+  async function sendRegistrationDetails(data) {
+    const postURL = process.env.NEXT_PUBLIC_POST_URL;
+    return await axios.post(postURL, data);
+  }
+  async function postData() {
+    let userObj = { email, username };
+    await sendRegistrationDetails(userObj);
+  }
 
-    function Register(){
-        createUserWithEmailAndPassword(email, password, username)
-        postData()
-    }
+  function Register() {
+    createUserWithEmailAndPassword(email, password, username);
+    postData();
+  }
 
   return (
-    <div>
-    <input
+    <div className="flex flex-col  items-center">
+      <input
         type="text"
         value={username}
         placeholder="username"
         onChange={(e) => setUsername(e.target.value)}
+        className="form-control"
       />
-    <input
+      <input
         type="email"
         value={email}
         placeholder="email"
         onChange={(e) => setEmail(e.target.value)}
+        className="form-control"
       />
       <input
         type="password"
         value={password}
         placeholder="password"
         onChange={(e) => setPassword(e.target.value)}
+        className="form-control"
       />
-      
-      <button onClick={() => Register}>
+
+      <button onClick={() => Register} className="register-button">
         Register
       </button>
-
-
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
