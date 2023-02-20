1c3fd8431b6bac43c697d08e3a0e6372c5139051
Fowl3r <90485594+Fowl3r@users.noreply.github.com>
2023-02-20 16:31:48 +0000

    styles

--- travel-frontend/pages/journal.js
+++ travel-frontend/pages/journal.js
@@ -5,7 +5,6 @@ const Journal = () => {
     <div className="min-h-screen ">
       <h1>Journal your travels</h1>
       <img  src="https://img.i-scmp.com/cdn-cgi/image/fit=contain,width=1098,format=auto/sites/default/files/styles/1200x800/public/images/methode/2018/04/10/6d76192e-3bdc-11e8-b6d9-57447a4b43e5_1280x720_110114.JPG?itok=gfu_JyUK" />
-      <Footer />
     </div>
   );
 };
