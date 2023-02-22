50dfbd8eb5ecf9024df97846df2011af0be4c030
Fowl3r <90485594+Fowl3r@users.noreply.github.com>
2023-02-22 22:02:24 +0000

    cards styled using absolute positioning on internal divs

--- travel-frontend/components/ApiResultCard.js
+++ travel-frontend/components/ApiResultCard.js
@@ -24,27 +24,28 @@ function ApiResultCard({
  
   
     return (
-      <div className="group card-containerR ">
-        <div className="single-cardR ">
-        <img src={image} className="card-imageR " />
+      <div className="group card-container ">
+        <div className="single-card ">
+        <img src={image} className="card-image " />
         </div>
-            <div className=" card-gradient-bgR">
-          <div className=" card-info-transitionR">
-              <h3 className="card-titleR ">{title}</h3>
-              <p className="card-cityR">{suburb +',  ' +city}</p>
-        <p className="card-countryR">{country}</p>
-              {/* <p className="card-suburbR">{suburb}</p> */}
-              <p className="card-descR">{description}</p>
-                <div className='card-action-containerR'>
-                  <button className="card-btnR">
+            <div className=" card-gradient-bg">
+          <div className=" card-info-transition">
+              <h3 className="card-title ">{title}</h3>
+              <p className="card-city">{suburb +',  ' +city}</p>
+        <p className="card-country">{country}</p>
+        <div className= 'desc-div'>
+              <p className="card-desc">{description}</p>
+        </div>
+                <div className='card-action-container'>
+                  <button className="card-btn">
                     <a href={wikipedia} target="_blank">
                       See more
                     </a>
                   </button>
-                  <button className=" card-btnR">
+                  <button className=" card-btn">
                     <FavouritesButton props={newFavourite}  />
                   </button>
-    
+                 
           </div>
         </div>
         </div>
@@ -53,31 +54,5 @@ function ApiResultCard({
     );
   }
   
-  
-
-//   return (
-//     <div className="card-container group">
-//       <div className="single-card">
-//         <img src={image} className="card-image" />
-//       </div>
-//       <div className=" card-gradient-bg">
-//         <div className=" card-info-transition">
-//           <h3 className="card-title">{title}</h3>
-//           <p className="card-city">{city}</p>
-//           <p className="card-location">{country}</p>
-//           <p className="card-location">{suburb}</p>
-//           <p className="card-desc">{description}</p>
-
-//           <div className='card-action-container'>
-          
-//        <button className="card-btn"><a href={wikipedia} target="_blank">See more</a></button>
-
-//           <FavouritesButton props={newFavourite} />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default ApiResultCard;
