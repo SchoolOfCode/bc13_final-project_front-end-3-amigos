7f4934b2182dcd8e1b5b15b067704dca7135e7e3
jo <jyotshnacs@gmail.com>
2023-03-02 12:20:48 +0000

    delete request

--- travel-frontend/pages/journal.js
+++ travel-frontend/pages/journal.js
@@ -11,28 +11,31 @@ const Journal = () => {
   const [journals, setJournals] = useState([]);
 
   useEffect(() => {
-      if (user) {
-        getJournalData()   
-     }
-    
+    if (user) {
+      getJournalData();
+    }
   }, [user]);
 
   async function getJournalData() {
-      const URL = `${process.env.NEXT_PUBLIC_POST_JOURNAL_URL}/${user.uid}`;
-      const entries = await axios.get(URL);
-      setJournals(entries.data.payload);
-    
+    const URL = `${process.env.NEXT_PUBLIC_JOURNAL_URl}/${user.uid}`;
+    const entries = await axios.get(URL);
+    setJournals(entries.data.payload);
   }
   async function postNewEntry(newJournal) {
-    const URL = `${process.env.NEXT_PUBLIC_POST_JOURNAL_URL}/${user.uid}`;
+    const URL = `${process.env.NEXT_PUBLIC_JOURNAL_URl}/${user.uid}`;
     let res = await axios.post(URL, newJournal);
-    await getJournalData()
+    await getJournalData();
     return res;
+  }
+
+  async function deleteJournalEntry(id) {
+    const URl = `${process.env.NEXT_PUBLIC_JOURNAL_URl}/${user.uid}`;
+    let res = await axios.delete(URL);
   }
 
   return (
     <div>
-      {user && <JournalForm user={user} postNewEntry={postNewEntry}/>}
+      {user && <JournalForm user={user} postNewEntry={postNewEntry} />}
       <JournalDataDisplay dataJournal={journals} />
     </div>
   );
