89290ffb4d1b1e1fc71e6f28e9ea0239a7cc3cc7
jo <jyotshnacs@gmail.com>
2023-03-06 12:39:54 +0000

    commented the console logs

--- travel-frontend/components/Journal/JournalCard.js
+++ travel-frontend/components/Journal/JournalCard.js
@@ -5,7 +5,7 @@ function JournalCard({ title, location, date, text, id, deleteEntry }) {
   // console.log("DATA AT JOUNRAL CARD:", id);
 
   return (
-    <div className=" card-display bg-red-500 container my-2 px-2 md:px-2">
+    <div className="card-display bg-red-500 container my-2 px-2 md:px-2">
       {/* <div className="flex flex-wrap mx-1 lg:-mx-4">
         <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-2 lg:w-1/3">
      <article className="overflow-hidden rounded-lg shadow-lg"> */}
