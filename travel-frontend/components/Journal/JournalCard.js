31b294193a7599999cf5ed6796cbed2cf924cc30
jo <jyotshnacs@gmail.com>
2023-03-07 11:16:44 +0000

    Sorting

--- travel-frontend/components/Journal/JournalCard.js
+++ travel-frontend/components/Journal/JournalCard.js
@@ -6,9 +6,6 @@ function JournalCard({ title, location, date, text, id, deleteEntry }) {
 
   return (
     <div className="card-display bg-red-500 container my-2 px-2 md:px-2">
-      {/* <div className="flex flex-wrap mx-1 lg:-mx-4">
-        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-2 lg:w-1/3">
-     <article className="overflow-hidden rounded-lg shadow-lg"> */}
       <div>
         <header className="flex items-left justify-between leading-tight p-2 md:p-4">
           <h2 className="text-lg">{title}</h2>
@@ -24,9 +21,6 @@ function JournalCard({ title, location, date, text, id, deleteEntry }) {
           }}
         />
       </div>
-      {/* </article>
-        </div>
-      </div> */}
     </div>
   );
 }
